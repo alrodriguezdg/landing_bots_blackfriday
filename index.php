d653d2cefc15a701e3e04d28a3b2c91382470696
Alejandro <alrodriguezdg@gmail.com>
2020-11-19 15:33:18 -0500

    Landing Page Botslovers Black Friday

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="utf-8">
+	<title>Botslovers Digital - Black Friday</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta name="description" content="Desarrollamos Software a la medida de las necesidades de su compañía.">
+	<!-- Favicon Web -->
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+
+	<!-- General Css Styles -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+	<header>
+		<div class="wrapper main-header">
+			<div class="row container">
+				<div class="col col-md-12">
+					<div class="logocont">
+						<div class="logostop">
+							<a href="https://botsloversdigital.com/" target="_blank">
+								<img src="images/logo-botslovers.png" alt="Botslovers" class="logoimg">
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="wrapper">
+		<!--banner-->
+		<div class="banner-botslovers">
+			<div class="row container">
+				<div class="col-md-6">
+					<h2>
+						Obtén tu <span>BOT</span> en <b>BLACK FRIDAY</b>
+					</h2>
+					<p>
+						Atiende todas las <b>solicitudes de tus clientes</b> en la mejor temporada del año
+					</p>
+					<button type="button" class="btn btn-red" data-toggle="modal" data-target="#exampleModalCenter">
+					  Comenzar 15 días GRATIS, luego US9.99 al mes
+					</button>
+				</div>
+				<div class="col-md-6 text-center">
+					<img src="images/main-image-banner.png">
+				</div>
+			</div>
+			<img class="divider-botslovers" src="images/divider-botslovers.png">		
+		</div>
+		<!--fin banner-->
+		<!--Contenido-->
+		<div class="section-botslovers">
+			<div class="row container">
+				<div class="col-md-6 align-self-center">
+					<img src="images/imagen-botslovers-blackfriday.png">
+				</div>
+				<div class="col-md-6 align-self-center">
+					<p>
+						En <b>BotsLovers,</b> te ayudamos a automatizar las preguntas habituales que recibes en tu ecommerce.
+					</p>
+					<p>Por medio de los canales donde se encuentran tus clientes.
+					</p>
+					<button type="button" class="btn btn-red" data-toggle="modal" data-target="#exampleModalCenter">
+					  Comenzar 15 días GRATIS, luego US9.99 al mes
+					</button>
+				</div>
+			</div>
+			<div class="row container">
+				<div class="col-md-6 align-self-center">
+					<p>
+						En <b>BotsLovers</b> somos referentes multiindustria hemos trabajado con <b>grandes marcas en Iberoamérica, tu compañía puede ser la próxima. </b>
+					</p>
+					<button type="button" class="btn btn-red" data-toggle="modal" data-target="#exampleModalCenter">
+					  Comenzar 15 días GRATIS, luego US9.99 al mes
+					</button>
+				</div>
+				<div class="col-md-6 align-self-center img-logos">
+					<img src="images/logo-botslovers-08.png">
+					<img src="images/logo-botslovers-09.png">
+					<img src="images/logo-botslovers-10.png">
+					<img src="images/logo-botslovers-11.png">
+				</div>
+			</div>
+		</div>
+		<!-- Fin Contenido-->
+	</div>
+	<footer>
+		<div class="divider-gradient">
+		</div>
+		<div class="row container">
+			<div class="col-md-12">
+				<ul class="contact-footer">
+  					<li><b>Contáctenos:</b></li>
+  					<li><b>MADRID:</b> Tel. +34 - 658 568 561 hola@botslovers.com Calle Joaquín Turina 2, Oficina 6 - Pozuelo de Alarcón</li>
+  					<li><b>BOGOTÁ:</b> Tel. +057 - 300 2363266 Javierv@botslovers.com - Calle. 81 #11-08</li>
+  				</ul>
+			</div>
+			<div class="col-md-6 align-self-center">
+				<p>
+					LEGAL<br>POLITICA DE PRIVACIDAD - CONDICIONES DE SERVICIO
+				</p>
+			</div>
+			<div class="col-md-6 align-self-center slogan text-right">
+				<ul class="social-botslovers">
+					<li>
+						<a target="_blank" href="https://www.facebook.com/botslovers/"><img src="images/img-landing-botslovers-09.png"></a>
+					</li>
+					<li>
+						<a target="_blank" href="https://twitter.com/andrespulgarine?lang=es"><img src="images/img-landing-botslovers-10.png"></a>
+					</li>
+					<li>
+						<a target="_blank" href="https://www.linkedin.com/company/11169982/admin/"><img src="images/img-landing-botslovers-11.png"></a>
+					</li>
+					<li>
+						<a target="_blank" href="https://www.youtube.com/channel/UCWyIEYtnmhv1_9mFBxRdsDA"><img src="images/img-landing-botslovers-12.png"></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Modal Cotización -->
+	<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+	  	<div class="modal-dialog modal-dialog-centered" role="document">
+		    <div class="modal-content">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          		<span aria-hidden="true">&times;</span>
+	        	</button>
+		      	<div class="modal-body text-center">
+		      		<div class="registro-step" id="registro-1">
+		      			<div class="content-registro-step">
+		      				<p><b>Genial,</b> vamos a empezar a <b>construir tu BOT</b> Para empezar por favor diligenciar los datos</p>
+				        	<div class="row form-bots">
+								<div class="col-md-12 col-sm-12 align-self-center">
+									<form>
+										<div class="form-group">
+									    	<input type="text" class="form-control" id="nombre" placeholder="Nombre">
+									  	</div>
+									  	<div class="form-group">
+									    	<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail">
+									  	</div>
+									  	<div class="form-group">
+									    	<input type="text" class="form-control" id="phone" placeholder="Teléfono">
+									  	</div>
+									  	<button type="submit" class="btn btn-red" id="boton-registro-1">Continuar</button>
+									</form>
+								</div>
+							</div>
+		      			</div>
+		      		</div>
+		      		<div class="registro-step" id="registro-2">
+		      			<div class="content-registro-step">
+		      				<p><b>Para finalizar,</b> tu me das FAQs y yo <b>te envio un BOT web.</b></p>
+			      			<h4>¡TE SORPRENDERA!</h4>
+			      			<hr>
+			      			<p>Puedes cargar preguntas de manera rápida <a href="#">Ver Plantilla ></a></p>
+			      			<div>
+			      				<a href="#" class="btn btn-outline-dark">Cargar preguntas</a>
+								<a href="#" class="btn btn-outline-dark">En qué página web estará el bot</a>
+			      			</div>
+			      			<button type="submit" class="btn btn-red" id="boton-registro-2">Finalizar</button>
+		      			</div>
+		      			<div class="footer-registro">
+		      				Comenzar 15 días GRATIS, luego US9.99 al mes
+		      			</div>
+		      		</div>
+		      		<div class="registro-step" id="registro-3">
+		      			<div class="content-registro-step">
+		      				<h3>¡FELICIDADES!</h3>
+			      			<p>En este momento nuestra fábrica de Bots <b>empezo a trabajar para crear tu asistente.</b></p>
+			      			<p><b>Solo una última cosa,</b> te envie un email, por favor <b>validalo</b> ya que allí <b>te indicaremos cuando tu BOT este listo.</b></p>
+			      			<a href="#" class="btn btn-outline-dark">Si no recibiste el email dale click a este botón.</a>
+		      			</div>
+		      			<div class="footer-registro">
+		      				<ul>
+		      					<li>Contáctenos:</li>
+		      					<li>MADRID: Tel. +34 - 658 568 561 hola@botslovers.com Calle Joaquín Turina 2, Oficina 6 - Pozuelo de Alarcón</li>
+		      					<li>BOGOTÁ: Tel. +057 - 300 2363266 Javierv@botslovers.com - Calle. 81 #11-08</li>
+		      				</ul>
+		      			</div>
+		      		</div>
+		      	</div>
+		    </div>
+		 </div>
+	</div>
+
+	<!-- LOADER SPECIAL-->		
+	<section id="loader_special">
+		<div class="cont_loader">
+			<div class="cart_loader">
+				<img src="images/elements/oval.svg" />
+				<p class="expecial_txt_loader">Cargando...</p>
+			</div>
+		</div>
+	</section>
+
+	<!-- Alertas -->
+	<div class="alerts_forms">
+		<div id="alertFailValidation" class="alertFail" >
+			<span> Por favor, ingrese todos los datos requeridos </span>
+		</div>
+		<div id="alertFailSend" class="alertFail" >
+			<span> Error al enviar mensaje, por favor intente más tarde  </span>
+		</div>
+		<div class="alertOk alertContacto" >
+			<span> Registro exitoso, pronto recibiras los detalles. ¡Gracias! </span>
+		</div>
+	</div>
+
+		<!-- General Scripts -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<!-- Contact Form -->
+	<script src="js/jquery.form.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Ajax  -->
+	<script type="text/JavaScript" src="js/ajax-woow.js"></script>
+	
+	<!-- FONT -->
+	<script src="js/all.min.js"></script>
+	<script type="text/javascript">
+		$('#myModal').on('shown.bs.modal', function () {
+		  $('#myInput').trigger('focus')
+		})
+
+		$( "#boton-registro-1" ).click(function() {
+		  $( "#registro-1" ).slideToggle();
+		  $( "#registro-2" ).slideToggle();
+		});
+		$( "#boton-registro-2" ).click(function() {
+		  $( "#registro-2" ).slideToggle();
+		  $( "#registro-3" ).slideToggle();
+		});
+
+	</script>
+
+</body>
+</html>
